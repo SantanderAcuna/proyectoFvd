ee0c8be5e7621847ff757b7832f45a6f0f602517
santander acuña <panson@misena.edu.co>
2020-09-14 11:35:55 -0500

    Se crea los archivos para subir proyecto a heroku

--- app/Http/Controllers/ReporteController.php
+++ app/Http/Controllers/ReporteController.php
@@ -22,7 +22,7 @@ class ReporteController extends Controller
      */
     public function __construct()
     {
-        $this->middleware('auth');
+        $this->middleware(['auth','roles']);
     }
 
 

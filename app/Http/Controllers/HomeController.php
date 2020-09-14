cf996d78f020d69c3a9d18f40a5dff98edeb52ba
santander acuña <panson@misena.edu.co>
2020-09-13 19:24:56 -0500

    Se crea los archivos para subir proyecto a heroku

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -17,7 +17,7 @@ class HomeController extends Controller
      */
     public function __construct()
     {
-        $this->middleware('auth');
+        $this->middleware('auth')->except('index');
 
         // solo quiero saber si funciona
     }

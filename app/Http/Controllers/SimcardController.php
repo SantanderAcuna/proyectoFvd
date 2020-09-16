750e6251a18eee7d39057e84b2d662ebf2933172
santander acuña <panson@misena.edu.co>
2020-09-16 17:48:53 -0500

    Se valida campo de archivo antes de subir al servidor

--- app/Http/Controllers/SimcardController.php
+++ app/Http/Controllers/SimcardController.php
@@ -53,10 +53,10 @@ class SimcardController extends Controller
             'file' => 'required'
         ]);
 
-        $file = $request->file('file')->getRealPath();
-        $path=storage_path('app').'/'.$file;  
+        //$file = $request->file('file')->getRealPath();
+        //$path=storage_path('app').'/'.$file;  
 
-        $data = Excel::import(new SimcardImport, $file, $path);
+        Excel::import(new SimcardImport,request()->file('file'));
         //Excel::import(new SimcardImport, request()->file('Inventario1.xlsx'));
         //(new SimcardImport)->import($file, null, \Maatwebsite\Excel\Excel::XLSX);
 

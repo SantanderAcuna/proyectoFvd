0788824ee94cfbbef7ac5627d9f92fe788a3be5c
santander acuña <panson@misena.edu.co>
2020-09-16 23:13:52 -0500

    Se crea una vista para ue los asesores puedan ver las ventas que realizaron

--- routes/web.php
+++ routes/web.php
@@ -22,48 +22,52 @@ Route::resource('revenue', 'RevenueController');
 Route::resource('simcard', 'SimcardController');
 
 
-Route::post('/', 'SimcardController@importarSimcard')->name('importar.simcard')->middleware(['roles']);
-Route::post('simcard', 'SimcardController@store')->name('simcard.store');
-Route::get('simcard', 'SimcardController@index')->name('simcard.index');
-Route::put('simcard/{simcard}', 'SimcardController@update')->name('simcard.update')->middleware(['roles']);
-Route::get('simcard/{simcard}/edit', 'SimcardController@edit')->name('simcard.edit')->middleware(['roles']);
 
-Route::get('asesor', 'ExcelController@misVentas')->name('mis.ventas');
-Route::get('usuario', 'HomeController@mostrar')->name('listar.usaurios')->middleware(['roles']);
-Route::delete('usuario/{id}', 'HomeController@destroy')->name('user.delete')->middleware(['roles']);
-Route::get('/usuario/{usuario}', 'HomeController@edit')->name('user.edit')->middleware(['roles']);
-Route::put('usuario/{id}', 'HomeController@update')->name('user.update')->middleware(['roles']);
+Route::middleware(['auth', 'roles'])->group(function () {
+  
+    Route::post('/', 'SimcardController@importarSimcard')->name('importar.simcard');
 
-//Route::get('reporte', 'ReporteController@ventas')->name('reporte.ventas')->middleware(['roles']);
-Route::get('/', 'ReporteController@ventas')->name('reporte.ventas')->middleware(['roles']);
-Route::get('reporte/export/', 'ReporteController@export')->name('export')->middleware(['roles']);
-Route::get('reporte', 'ReporteController@index')->name('reporte.index')->middleware(['roles']);
-Route::post('reporte', 'ReporteController@store')->name('reporte.store');
-Route::get('reporte/{reporte}/edit', 'ReporteController@edit')->name('reporte.edit')->middleware(['roles']);
-Route::delete('reporte/{reporte}', 'ReporteController@destroy')->name('reporte.destroy')->middleware(['roles']);
+    Route::put('simcard/{simcard}', 'SimcardController@update')->name('simcard.update');
+    Route::get('simcard/{simcard}/edit', 'SimcardController@edit')->name('simcard.edit');
+    
+    
+    Route::get('usuario', 'HomeController@mostrar')->name('listar.usaurios');
+    Route::delete('usuario/{id}', 'HomeController@destroy')->name('user.delete');
+    Route::get('/usuario/{usuario}', 'HomeController@edit')->name('user.edit');
+    Route::put('usuario/{id}', 'HomeController@update')->name('user.update');
+    
 
-Route::get('producto', 'ProductoController@index')->name('producto.index')->middleware(['roles']);
-Route::post('producto', 'ProductoController@store')->name('producto.store')->middleware(['roles']);
-Route::get('producto/{producto}/edit', 'ProductoController@edit')->name('producto.edit')->middleware(['roles']);
-Route::delete('producto/{producto}', 'ProductoController@destroy')->name('producto.destroy')->middleware(['roles']);
-Route::put('producto/{producto}', 'ProductoController@update')->name('producto.update')->middleware(['roles']);
-
-Route::get('tipo', 'TipoVentaController@index')->name('tipo.index')->middleware(['roles']);
-Route::post('tipo', 'TipoVentaController@store')->name('tipo.store')->middleware(['roles']);
-Route::get('tipo/{tipo}/edit', 'TipoVentaController@edit')->name('tipo.edit')->middleware(['roles']);
-Route::delete('tipo/{tipo}', 'TipoVentaController@destroy')->name('tipo.destroy')->middleware(['roles']);
-Route::put('tipo/{tipo}', 'TipoVentaController@update')->name('tipo.update')->middleware(['roles']);
-
-
-
-//Route::get('reporte', 'ReporteController@index')->name('reporte.index')->middleware(['roles']);
-//Route::post('reporte', 'ReporteController@store')->name('reporte.store');
-//Route::get('reporte/{reporte}/edit', 'ReporteController@edit')->name('reporte.edit')->middleware(['roles']);
-//Route::delete('reporte/{reporte}', 'ReporteController@destroy')->name('reporte.destroy')->middleware(['roles']);
+    Route::get('/', 'ReporteController@ventas')->name('reporte.ventas');
+    Route::get('reporte/export/', 'ReporteController@export')->name('export');
+    Route::get('reporte', 'ReporteController@index')->name('reporte.index');
+    Route::post('reporte', 'ReporteController@store')->name('reporte.store');
+    Route::get('reporte/{reporte}/edit', 'ReporteController@edit')->name('reporte.edit');
+    Route::delete('reporte/{reporte}', 'ReporteController@destroy')->name('reporte.destroy');
+    
+    Route::get('producto', 'ProductoController@index')->name('producto.index');
+    Route::post('producto', 'ProductoController@store')->name('producto.store');
+    Route::get('producto/{producto}/edit', 'ProductoController@edit')->name('producto.edit');
+    Route::delete('producto/{producto}', 'ProductoController@destroy')->name('producto.destroy');
+    Route::put('producto/{producto}', 'ProductoController@update')->name('producto.update');
+    
+    Route::get('tipo', 'TipoVentaController@index')->name('tipo.index');
+    Route::post('tipo', 'TipoVentaController@store')->name('tipo.store');
+    Route::get('tipo/{tipo}/edit', 'TipoVentaController@edit')->name('tipo.edit');
+    Route::delete('tipo/{tipo}', 'TipoVentaController@destroy')->name('tipo.destroy');
+    Route::put('tipo/{tipo}', 'TipoVentaController@update')->name('tipo.update');
+});
 
 
-
+// Ruta de asesores
 
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+Route::get('asesor', 'ExcelController@misVentas')->name('mis.ventas');
+Route::post('simcard', 'SimcardController@store')->name('simcard.store');
+Route::get('simcard', 'SimcardController@index')->name('simcard.index');
+
+
+
+
+

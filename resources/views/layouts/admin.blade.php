f303aa6ffe2226c55b5b663c02b8650a9af8204d
santander acuña <panson@misena.edu.co>
2020-09-18 05:32:30 -0500

    Se crea una vista para que los suspervisores puedan ver los reportes por dia

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -44,8 +44,7 @@
                 <a id="navbarDropdown" class="nav-link dropdown-toggle text-white" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
                     {{ Auth::user()->name }}
                 </a>
-                <div class="dropdown-divider"></div>
-
+               
                 <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
                     <a class="dropdown-item text-primary" href="{{ route('logout') }}" onclick="event.preventDefault();
                                          document.getElementById('logout-form').submit();">

31deff518e4d6a9f240c9487369a57ef9e813185
santander acuña <panson@misena.edu.co>
2020-09-18 05:31:20 -0500

    Se crea una vista para que los suspervisores puedan ver los reportes por dia

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -44,27 +44,11 @@
                 <a id="navbarDropdown" class="nav-link dropdown-toggle text-white" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
                     {{ Auth::user()->name }}
                 </a>
+                <div class="dropdown-divider"></div>
 
-
-            </li>
-           
-
-            <div class="dropdown-divider"></div>
-
-            <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                                <a class="dropdown-item text-primary" href="{{ route('logout') }}" onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                    <strong>{{ __('Salir') }}</strong>
-                                </a>
-
-                                <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-                                    @csrf
-                                </form>
-                            </div>
-            <a class="dropdown-item" href="login.html">
                 <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
                     <a class="dropdown-item text-primary" href="{{ route('logout') }}" onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
+                                         document.getElementById('logout-form').submit();">
                         <strong>{{ __('Salir') }}</strong>
                     </a>
 
@@ -72,8 +56,7 @@
                         @csrf
                     </form>
                 </div>
-            </a>
-            </div>
+
             </li>
         </ul>
         @endauth
